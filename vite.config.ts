53397de6ab02cb513917aae770a68e5c130c65eb
Igor <zhuravkovigor@icloud.com>
2025-05-19 10:25:12 +0500

    Changed directory from dist name to a build for deploy purpose

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-			},
-		}),
-	],
+  plugins: [
+    preact({
+      prerender: {
+        enabled: true,
+        renderTarget: "#app",
+      },
+    }),
+  ],
+
+  build: {
+    outDir: "./build",
+  },
 });
